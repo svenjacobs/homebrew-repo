967873893c6bdad35effa1786e1e54e4ac75c4b9
Sven Jacobs <github@svenjacobs.com>
2020-07-23 12:30:39 +0200

    Update cask according to styleguide

--- Casks/function-pilot.rb
+++ Casks/function-pilot.rb
@@ -1,17 +1,16 @@
-cask 'function-pilot' do
+cask "function-pilot" do
   version :latest
   sha256 :no_check
 
-  url 'http://tinybird.com/download/FunctionPilot.dmg'
-  name 'Function Pilot'
-  homepage 'http://tinybird.com/mac/function-pilot.html'
+  url "http://tinybird.com/download/FunctionPilot.dmg"
+  name "Function Pilot"
+  homepage "http://tinybird.com/mac/function-pilot.html"
 
-  app 'Function Pilot.app'
+  app "Function Pilot.app"
 
-  zap trash: [
-    '~/Library/Application Support/FunctionPilot', 
-    '~/Library/Preferences/com.tinybird.functionpilot.plist', 
-    '~/Library/Caches/com.tinybird.functionpilot'
+  zap :trash => [
+    "~/Library/Application Support/FunctionPilot",
+    "~/Library/Preferences/com.tinybird.functionpilot.plist",
+    "~/Library/Caches/com.tinybird.functionpilot",
   ]
 end
-
