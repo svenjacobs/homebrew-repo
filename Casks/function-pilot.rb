128f5973b5a973fd5cafec4916359be975960a49
Sven Jacobs <github@svenjacobs.com>
2020-07-23 12:06:48 +0200

    Add cask for Function Pilot

---
+++ Casks/function-pilot.rb
@@ -0,0 +1,17 @@
+cask 'function-pilot' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://tinybird.com/download/FunctionPilot.dmg'
+  name 'Function Pilot'
+  homepage 'http://tinybird.com/mac/function-pilot.html'
+
+  app 'Function Pilot.app'
+
+  zap trash: [
+    '~/Library/Application Support/FunctionPilot', 
+    '~/Library/Preferences/com.tinybird.functionpilot.plist', 
+    '~/Library/Caches/com.tinybird.functionpilot'
+  ]
+end
+
